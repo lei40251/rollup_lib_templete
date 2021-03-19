4f9b98c007545db64e0edbb16d14cdb3540b4715
zhaolei <lei40251@163.com>
2021-03-20 01:39:09 +0800

    v2.0.0

---
+++ build/version.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+const fs = require('fs');
+const path = require('path');
+const pathname = path.resolve(__dirname, '../package.json');
+const pkg = JSON.parse(fs.readFileSync(pathname, 'utf-8'));
+const version = pkg.version;
+
+console.log('version:', version);
+
+const exec = require('child_process').exec;
+const cmdStr = `git commit -m "v${version}" && git push && git tag -a "v${version}" -m "${version}" && git push origin --tags`;
+
+exec(cmdStr, function(err, stdout, stderr)
+{
+  console.log('exec:', err, stdout, stderr);
+});
