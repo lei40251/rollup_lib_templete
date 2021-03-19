146145f47227b16720381347b9073d5ed81e1a02
zhaolei <lei40251@163.com>
2021-03-20 01:29:13 +0800

    New:eslint

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,29 +1,31 @@
 import { babel } from '@rollup/plugin-babel';
+import eslint from '@rollup/plugin-eslint';
 import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 import { version } from '../package.json';
 
 export default {
-  input: 'src/utils.ts',
-  output: {
-    name: 'my_utils',
-    file: `dist/@${version}/index.js`,
-    format: 'umd',
-    sourcemap: true,
+  input  : 'src/utils.ts',
+  output : {
+    name      : 'my_utils',
+    file      : `dist/@${version}/index.js`,
+    format    : 'umd',
+    sourcemap : true
   },
-  plugins: [
+  plugins : [
+    eslint(),
     typescript({
-      tsconfigOverride: {
-        compilerOptions: {
-          declaration: false,
-        },
-      },
+      tsconfigOverride : {
+        compilerOptions : {
+          declaration : false
+        }
+      }
     }),
     babel({
-      extensions: [".js", ".ts"],
-      exclude: "node_modules/**",
-      babelHelpers: 'bundled'
+      extensions   : [ '.js', '.ts' ],
+      exclude      : 'node_modules/**',
+      babelHelpers : 'bundled'
     }),
-    terser(),
-  ],
+    terser()
+  ]
 };
